cb4689ffd5f9e0ee50a1264b6dcac57001934475
Luis <carcamocariasl@yahoo.com>
2023-04-18 19:20:31 -0600

    modificar al guardar

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,86 +1,82 @@
-import { useDispatch, useSelector } from "react-redux"
-import Swal from "sweetalert2";
-import calendarApi from "../../api/calendarApi";
-import { convertEventToDateEvents } from "../helpers";
+import { useDispatch, useSelector } from 'react-redux'
+import Swal from 'sweetalert2'
+import calendarApi from '../../api/calendarApi'
+import { convertEventToDateEvents } from '../helpers'
 
-import {onSetActiveEvent,onAddNewEvent, onUpdateEvent,onDeleteEvent,onLoadEvents} from '../store'
+import {
+  onSetActiveEvent,
+  onAddNewEvent,
+  onUpdateEvent,
+  onDeleteEvent,
+  onLoadEvents,
+} from '../store'
 
 export const useCalendarStore = () => {
-    const {events,activeEvent}=useSelector(state=>state.calendar);
-    const {user}=useSelector(state=>state.auth);
-    const dispatch=useDispatch();
+  const { events, activeEvent } = useSelector((state) => state.calendar)
+  const { user } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
 
-    const setActiveEvent=(calendarEvent)=>{
-        dispatch(onSetActiveEvent(calendarEvent))
-    }
+  const setActiveEvent = (calendarEvent) => {
+    dispatch(onSetActiveEvent(calendarEvent))
+  }
 
-    const startSavingEvent= async(calendarEvent)=>{
-        //TODO: LLegar al backend
+  const startSavingEvent = async (calendarEvent) => {
+    //TODO: LLegar al backend
 
-        //todo bien
+    //todo bien
 
-        try {
-          
-            if(calendarEvent.id){
-               //Actualizando evento
-               await calendarApi.put(`/events/${calendarEvent.id}`,calendarEvent);
-               
-               dispatch(onUpdateEvent({...calendarEvent,user}));
-               return;
-               
-             }
-          
-               //Creando evento
-               const {data} =await calendarApi.post('/events',calendarEvent);
-               dispatch(onAddNewEvent({...calendarEvent,id:data.evento.id,user:user}));
-                  
-        } catch (error) {
-          console.log(error)
-          Swal.fire('Error al guardar',error.response.data.msg,'error')
-          
-        }
-    }
+    try {
+      if (calendarEvent?.id) {
+        //Actualizando evento
+        await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent)
 
-    const startDeleteEvent= async()=>{
-      //TODO: llegar al backed
-
-      try {
-          await calendarApi.delete(`/events/${activeEvent.id}`)
-          dispatch(onDeleteEvent())
-        
-      } catch (error) {
-        console.log(error)
-        Swal.fire('Error al eliminar',error.response.data.msg,'error')
-        
-      }
-    }
-
-
-    const startLoadingEvent= async()=>{
-      try {
-        const {data}= await calendarApi.get('/events');
-        const events= convertEventToDateEvents(data.msg);
-       
-        dispatch(onLoadEvents(events))
-      } catch (error) {
-        console.log(error)
-        
+        dispatch(onUpdateEvent({ ...calendarEvent, user }))
+        return
       }
 
+      //Creando evento
+      const { data } = await calendarApi.post('/events', calendarEvent)
+      dispatch(
+        onAddNewEvent({ ...calendarEvent, id: data?.evento.id, user: user })
+      )
+    } catch (error) {
+      console.log(error)
+      Swal.fire('Error al guardar', error.response?.data?.msg, 'error')
     }
-  return{
+  }
+
+  const startDeleteEvent = async () => {
+    //TODO: llegar al backed
+
+    try {
+      await calendarApi.delete(`/events/${activeEvent.id}`)
+      dispatch(onDeleteEvent())
+    } catch (error) {
+      console.log(error)
+      Swal.fire('Error al eliminar', error.response?.data?.msg, 'error')
+    }
+  }
+
+  const startLoadingEvent = async () => {
+    try {
+      const { data } = await calendarApi.get('/events')
+      const events = convertEventToDateEvents(data?.msg)
+
+      dispatch(onLoadEvents(events))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return {
     //*Propiedades
     events,
     activeEvent,
-    hasEventSelected:!!activeEvent,
-
-
+    hasEventSelected: !!activeEvent,
 
     //*Metodos
     setActiveEvent,
     startSavingEvent,
     startDeleteEvent,
-    startLoadingEvent
-
+    startLoadingEvent,
   }
 }
